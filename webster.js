e907f8430f45185ce836bb9d59de1078cb562320
vstesh <stesh@ukr.net>
2021-12-17 13:36:33 +0200

    fix

--- webster.js
+++ webster.js
@@ -16,7 +16,7 @@ app.all('*', function(req, res, next) {
     res.header('Access-Control-Allow-Origin', origin);
     res.header('Access-Control-Allow-Origin', "*");
     res.header("Access-Control-Allow-Headers", "X-Requested-With");
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    res.header('Access-Control-Allow-Headers', 'Conte nt-Type');
     next();
 });
 
