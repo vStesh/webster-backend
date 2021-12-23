554e342a329e02d85cc7a4b61a46079a370d099d
Ярослав Миронюк <ymyroniuk@e2r3p3.unit.ua>
2021-12-23 18:16:45 +0200

    update photo model

--- models/Photo.js
+++ models/Photo.js
@@ -3,6 +3,7 @@ const { Schema, model } = require('mongoose');
 const PhotoSchema = new Schema({
     user: { type: Schema.Types.ObjectId, ref: 'User' },
     url: { type: String, required: true },
+    settings: {type: Object, required: true},
     comment: { type: String, required: true },
     createdAt: {type: Date, default: Date.now},
     updatedAt: { type: Date, default: Date.now },
