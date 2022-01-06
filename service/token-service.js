3fdda03558d1b873b44dea6ffd28c81fe0d48ec5
Vladimir <stesh@ukr.net>
2022-01-06 12:44:10 +0200

    token expired 1m

--- service/token-service.js
+++ service/token-service.js
@@ -4,7 +4,7 @@ const {JWT_ACCESS_SECRET, JWT_REFRESH_SECRET, JWT_RESET_SECRET} = require('../co
 
 class TokenService {
     generateTokens(payload) {
-        const accessToken = jwt.sign(payload, JWT_ACCESS_SECRET, { expiresIn: '30m' })
+        const accessToken = jwt.sign(payload, JWT_ACCESS_SECRET, { expiresIn: '1m' })
         const refreshToken = jwt.sign(payload, JWT_REFRESH_SECRET, { expiresIn: '30d' })
         return {
             accessToken,
