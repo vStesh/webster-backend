d0c1e5cb259e9090e78e72ab6afe4dce322311a4
Vladimir <stesh@ukr.net>
2022-01-07 22:34:47 +0200

    same site cookie

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -57,7 +57,7 @@ exports.login = async (req, res) => {
             const userDto = new UserDto(candidate)
             const tokens = tokenService.generateTokens({ ...userDto })
             await tokenService.saveToken(userDto.id, tokens.refreshToken);
-            res.cookie('refreshToken', tokens.refreshToken, { maxAge: 30 * 24 * 60 * 60 * 1000, httpOnly: true, domain: '.printapp.store', sameSite: 'none'})
+            res.cookie('refreshToken', tokens.refreshToken, { maxAge: 30 * 24 * 60 * 60 * 1000, httpOnly: true, domain: '.printapp.store'})
             return res.status(200).json(getRes(0, {data: { ...tokens, user: userDto }}))
         }
     } catch (err) {
@@ -149,7 +149,7 @@ exports.refresh = async (req, res) => {
         const userDto = new UserDto(user)
         const tokens = tokenService.generateTokens({ ...userDto })
         await tokenService.saveToken(userDto.id, tokens.refreshToken)
-        res.cookie('refreshToken', tokens.refreshToken, { maxAge: 30 * 24 * 60 * 60 * 1000, httpOnly: true, domain: '.printapp.store', sameSite: 'none'})
+        res.cookie('refreshToken', tokens.refreshToken, { maxAge: 30 * 24 * 60 * 60 * 1000, httpOnly: true, domain: '.printapp.store'})
         return res.status(200).json(getRes(0, { data: { ...tokens, user: userDto } }))
     } catch (err) {
         return res.status(500).json(getRes(100, {error: err.message}))
