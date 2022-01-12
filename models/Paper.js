2b4ccb17aa6cd6f53e04b23e81da26ab588c74cf
Vladimir <stesh@ukr.net>
2022-01-12 13:22:05 +0200

    change model paper

--- models/Paper.js
+++ models/Paper.js
@@ -4,7 +4,7 @@ const PaperSchema = new Schema({
     name: { type: String, required: true, unique: true },
     size: { type: Schema.Types.ObjectId, ref: 'Size', required: true },
     typePaper: { type: String, required: true },
-    density: {type: String, required: true},
+    density: {type: Number, required: true},
     createdAt: {type: Date, default: Date.now},
     updatedAt: { type: Date, default: Date.now },
     deletedAt: { type: Date, default: null }
